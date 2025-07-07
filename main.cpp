6230f31a98e1ec8d6e9b9582dd1a4aaf9fe57c81
Munkata <mpushev9@gmail.com>
2025-07-07 16:38:46 +0300

    visuals

--- main.cpp
+++ main.cpp
@@ -2,14 +2,49 @@
 #define WINDOW_X 800
 #define WINDOW_y 600
 int main() {
-    sf::Color beige(203, 190, 179);
-    sf::Color reddish(209, 73, 91);
+    sf::Color beige(203, 190, 179); //background
+    sf::Color reddish(209, 73, 91); //player_1
+    sf::Color grey(73, 71, 91); //player_2
     sf::RenderWindow window(sf::VideoMode(WINDOW_X, WINDOW_y), "My 2D Game");
-    sf::CircleShape player(25.0f); // Simple player circle
-    player.setFillColor(reddish);
-    float locationX=0;
-    float locationY=0;
-    player.setPosition(locationX, locationY);
+
+        // Create the border lines
+    sf::RectangleShape topLine(sf::Vector2f(WINDOW_X-2*10, 2));
+    topLine.setPosition(10, 10);
+    topLine.setFillColor(sf::Color::Black);
+
+    sf::RectangleShape bottomLine(sf::Vector2f(WINDOW_X-2*10, 2));
+    bottomLine.setPosition(10, WINDOW_y-10);
+    bottomLine.setFillColor(sf::Color::Black);
+
+    sf::RectangleShape leftLine(sf::Vector2f(2, WINDOW_y-2*10));
+    leftLine.setPosition(10, 10);
+    leftLine.setFillColor(sf::Color::Black);
+
+    sf::RectangleShape rightLine(sf::Vector2f(2, WINDOW_y-2*10));
+    rightLine.setPosition(WINDOW_X-10, 10);
+    rightLine.setFillColor(sf::Color::Black);
+
+    sf::RectangleShape centerline;
+    centerline.setSize(sf::Vector2f(2, WINDOW_y - 2*10));
+    centerline.setPosition(((WINDOW_X - 2) / 2) + 4, 10);
+    centerline.setFillColor(sf::Color::Black);
+
+    sf::CircleShape centercircle;
+    centercircle.setRadius(100);
+    centercircle.setPosition(WINDOW_X /2 -95, WINDOW_y / 2 -98 );
+    centercircle.setOutlineThickness(2);
+    centercircle.setOutlineColor(sf::Color::Black);
+    centercircle.setFillColor(sf::Color::Transparent);
+
+
+    sf::RectangleShape player_1(sf::Vector2f(20.f, 150.f));//width xheight
+    // sf::CircleShape player_1(25.0f); // Simple player_1 circle
+    player_1.setFillColor(reddish);
+    player_1.setPosition(5, 10);
+
+    sf::RectangleShape player_2(sf::Vector2f(20.f, 150.f));//width xheight
+    player_2.setFillColor(grey);
+    player_2.setPosition(WINDOW_X-5, 0);
 
     sf::CircleShape ball(10.0f);
     ball.setFillColor(sf::Color::Green);
@@ -23,25 +58,23 @@ int main() {
 
         // Movement
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-            player.move(0, -0.1);
-            locationY-=0.1;
+            player_1.move(0, -0.1);
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            player.move(0,  0.1);
-            locationY+=0.1;
-        
+            player_1.move(0,  0.1);
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-            player.move(-0.1, 0);
-            locationX-=0.1;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            player.move(0.1, 0);
-            locationX+=0.1;
-        }
-        printf("Location x:%.2f y:%.2f\n",locationX, locationY);
+
         window.clear(beige);
-        window.draw(player);
+    
+        window.draw(topLine);
+        window.draw(bottomLine);
+        window.draw(leftLine);
+        window.draw(rightLine);
+        window.draw(centercircle);
+        window.draw(centerline);
+
+        window.draw(player_1);
+        window.draw(player_2);
         window.draw(ball);//order of draw matters
         window.display();
     }
